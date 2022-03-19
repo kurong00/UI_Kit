3661ebce8f7342e2c330682ca0010ed170c66124
kurong00 <kurong00@gmail.com>
2022-03-19 22:59:37 +0800

    add RaycastTargetLine

---
+++ Assets/Script/RaycastTargetLine.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class RaycastTargetLine : MonoBehaviour
+{
+	static Vector3[] fourCorners = new Vector3[4];
+	void OnDrawGizmos()
+	{
+		foreach (MaskableGraphic g in GameObject.FindObjectsOfType<MaskableGraphic>())
+		{
+			if (g.raycastTarget)
+			{
+				RectTransform rectTransform = g.transform as RectTransform;
+				rectTransform.GetWorldCorners(fourCorners);
+				Gizmos.color = Color.red;
+				for (int i = 0; i < 4; i++)
+					Gizmos.DrawLine(fourCorners[i]+Vector3.one, fourCorners[(i + 1) % 4] + Vector3.one);
+			}
+		}
+	}
+}
